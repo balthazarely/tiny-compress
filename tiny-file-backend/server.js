7c6f392821ae9d108746e32c522d18e6ef3d603b
balthazarely <balthazar.elyj@gmail.com>
2026-05-06 08:47:19 -0600

    use dynamic PORT and 0.0.0.0 host for Railway

--- tiny-file-backend/server.js
+++ tiny-file-backend/server.js
@@ -24,7 +24,8 @@ fastify.get("/", async (request, reply) => {
 await fastify.register(compressRoutes);
 
 try {
-  await fastify.listen({ port: 3000 });
+  const port = process.env.PORT || 3000;
+  await fastify.listen({ port, host: "0.0.0.0" });
 } catch (err) {
   fastify.log.error(err);
   process.exit(1);
